be80d8f4f7425ff83fd227452520440e141793b0
Anna Maria <aannamariabr@outlook.com>
2022-10-10 14:26:28 -0300

    Configurando eslint

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,10 +1,10 @@
-export * from './colors'	
+export * from './colors'
 
-export * from './space'	
+export * from './space'
 
-export * from './radii'	
+export * from './radii'
 
 export * from './fonts'
 export * from './font-sizes'
-export * from './font-weights'			
+export * from './font-weights'
 export * from './line-heights'
