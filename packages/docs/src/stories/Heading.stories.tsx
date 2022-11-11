12238517e31520ee81536f10f3700e04d4ff9c7f
Anna Maria <aannamariabr@outlook.com>
2022-11-11 20:01:50 -0300

    Compoenente heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,27 @@
+import { Heading, HeadingProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom Heading',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'h1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão heading sempre setá `h2`, mas podemos customizar isso com a proprieadade `as`',
+      },
+    },
+  },
+}
